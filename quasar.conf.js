eac395c580823fa39ffc59ecab55fd1af8a41d6c
returntears <returntears@outlook.com>
2021-12-28 23:53:15 +0800

    second commit

--- quasar.conf.js
+++ quasar.conf.js
@@ -92,7 +92,7 @@ module.exports = configure(function (ctx) {
       // (like functional components as one of the examples),
       // you can manually specify Quasar components/directives to be available everywhere:
       //
-      // components: [],
+      components: ['QBtn'],
       // directives: [],
 
       // Quasar plugins
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,10 +1,20 @@
 <template>
   <q-page class="flex flex-center">
-    <img
+    <!-- <img
       alt="Quasar logo"
       src="~assets/quasar-logo-vertical.svg"
       style="width: 200px; height: 200px"
-    >
+    > -->
+    <div>
+      <q-btn label="New Item" @click="clickItem"/>
+      <q-btn icon="map"/>
+      <q-btn icon="create" label="New Item"/>
+      <q-btn round color="secondary" icon="card_giftcard" />
+      <q-btn round color="secondary">
+        <q-icon name="card_giftcard" />
+      </q-btn>
+      <span>{{ msg }}</span>
+    </div>
   </q-page>
 </template>
 
@@ -12,6 +22,16 @@
 import { defineComponent } from 'vue'
 
 export default defineComponent({
-  name: 'PageIndex'
+  name: 'PageIndex',
+  data () {
+    return {
+      msg: 'PageIndex'
+    }
+  },
+  methods: {
+    clickItem () {
+      alert('PageIndex')
+    }
+  }
 })
 </script>
